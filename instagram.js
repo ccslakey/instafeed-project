45ede2a3f119ea01d83d4cb5027da8136e4a87d0
ccslakey <cromwellslakey@gmail.com>
2015-11-17 12:17:03 -0600

    can't combine tags with instafeed lol

--- instagram.js
+++ instagram.js
@@ -1,16 +1,16 @@
 $(function(){
-	// console.log("your jQuery is ready sir");
 
-
-	var feed = new Instafeed({
+	var defaultOptions = {
+		limit: 50,
 	    resolution: "thumbnail",
 	    get: 'tagged',
 	    sortBy: "most-liked",
 	    tagName: 'funny',
 	    clientId: '90f3cb97b92b4ee0b847996ec7aa9264'
-	});
+	}
 
-	$("#get-photos").click(function() {
+	var assignTags = function () {
+		var feed = new Instafeed(defaultOptions);
 		// get user input
 		var inputTag = $("#search-box").val();
 		var defaultTag = feed.options.tagName;
@@ -19,8 +19,16 @@ $(function(){
 
 		// set feed to whatever we decided to use
 		feed.options.tagName = finalTag
-		console.log("the tag is: " + feed.options.tagName);
+		console.log("the tag is: " + feed.options.tagName);	
 		feed.run();	
+	}
+
+	$("#get-photos").click(function() {
+		
+		$("#instafeed").empty()
+
+		assignTags();
+		
 	});
 	
 	$("#clear-photos").click(function() {
