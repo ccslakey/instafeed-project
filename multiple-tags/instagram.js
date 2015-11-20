34f35c60382b049f9a980e289f66d5be97c50f72
ccslakey <cromwellslakey@gmail.com>
2015-11-20 11:13:50 -0600

    return some data from multiple hashtag search

--- multiple-tags/instagram.js
+++ multiple-tags/instagram.js
@@ -1,33 +1,52 @@
-$(function(){
- 
- $.ajax({
- 	url:"./credentials.js",
- 	type: "GET",
- 	crossDomain: true,
- 	dataType: "jsonp"
- }).done(function (data) {
- 	console.log(data);
- })
-	
-	
+$(function() {
+    var client_id = "90f3cb97b92b4ee0b847996ec7aa9264";
 
+    // decide whether to use a default 
+    // hashtag or get an array of input
+    var assignTags = function() {
+        // get user input
+        var inputArr = $("#search-box").val();
+        if(!inputArr){
+        	var defaultTag = "puppy";
+        } else{
+        	inputArr = inputArr.split(" ")
+        }
+        // if there is something we'll use it, if not use default
+        var hashtag = inputArr || defaultTag
+        return hashtag
+    }
 
-	$("#get-photos").click(function() {
-		
-		$("#instafeed").empty()
+    $("#get-photos").click(function() {
+        // $("#instafeed").empty()
+        var tags = assignTags();
+        
+        if (Array.isArray(tags)) {
+        	console.log("now searching instagram for " + tags.toString());
+      		// make several api calls with user input
+        	tags.forEach(function (tag) {
+        		callInstagramAPI(tag);
 
-		$.ajax({
-			url:"https://api.instagram.com/v1/tags/nofilter/media/recent?client_id="+client_id,
-			type: "GET",
-			crossDomain: true,
-			dataType: "jsonp"
-		}).done(function (data) {
-			console.log(data);
-		})
-		
-	});
-	
-	$("#clear-photos").click(function() {
-		$("#instafeed").empty()
-	});
+        	});
+      
+    	} else {
+    	// just use the default tag
+        console.log(tags);
+        callInstagramAPI(tags);
+        }
+    });
+
+    function callInstagramAPI(tag) {
+        $.ajax({
+            url: "https://api.instagram.com/v1/tags/" + tag + "/media/recent?&client_id=" + client_id,
+            type: "GET",
+            crossDomain: true,
+            dataType: "jsonp"
+        }).done(function(data) {
+            console.log(data);
+        });
+    }	
+
+    $("#clear-photos").click(function() {
+        $("#instafeed").empty()
+    });
 });
